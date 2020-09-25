3cce97add97e162393efd799bbd33de95d7955dd
rudiduc <rudiduc@gmail.com>
2020-09-26 11:22:38 +1200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,23 +6,26 @@
  */
 const Firestore = require('@google-cloud/firestore');
 const PROJECTID = 'equifax-hackathon-2020';
-const COLLECTION_NAME = 'microservice-data';
+const COLLECTION_NAME = 'function-test-data';
 const firestore = new Firestore({
-  projectId: PROJECTID,
-  timestampsInSnapshots: true,
+    projectId: PROJECTID,
+    timestampsInSnapshots: true,
 });
 
 exports.helloPubSub = (event, context) => {
-  const message = event.data
-    ? Buffer.from(event.data, 'base64').toString()
-    : 'No data!';
-    // store/insert a new document
+    const message = event.data ?
+        Buffer.from(event.data, 'base64').toString() :
+        'No data!';
+
     const created = new Date().getTime();
+    var myJsonObject = JSON.parse(message);
+    myJsonObject.created = created;
+
     return firestore.collection(COLLECTION_NAME)
-      .add( JSON.parse(message) )
-      .then(doc => {
-        console.log(doc);
-      }).catch(err => {
-        console.error(err);
-      });
+        .add(myJsonObject)
+        .then(doc => {
+            console.log(doc);
+        }).catch(err => {
+            console.error(err);
+        });
 };
